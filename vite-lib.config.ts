b5e4ba8b3885bcc3b936d2f22febc466d73d0bfc
risonakamo <hmykanae@gmail.com>
2025-07-20 17:28:21 -0400

    trying to get lib mode working

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -2,11 +2,14 @@ import {defineConfig} from "vite";
 import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 import {LogLevel,RollupLog,LogHandler} from "rollup";
-import {Warning} from "svelte/compiler";
-
-type SvelteWarningHandler=(warning:Warning)=>void
 
 declare const __dirname:string;
+
+interface TargetSettings
+{
+    name:string
+    path:string
+}
 
 export default defineConfig({
     mode:"development",
@@ -27,10 +30,9 @@ export default defineConfig({
     build:{
         lib:{
             name:"schelper",
-            fileName:"schelper",
+            fileName:"open-exh-full-size",
             entry:{
                 e1:`${__dirname}/web/content-scripts/open-exh-full-size.ts`,
-                e2:`${__dirname}/web/content-scripts/ex-page-controls.ts`,
             },
             formats:["iife"]
         },
