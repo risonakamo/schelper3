af2fd16e1ac3da67c26bfd7ee778d9236f4b739a
risonakamo <hmykanae@gmail.com>
2025-07-26 12:54:31 -0400

    reorganising

---
+++ web/lib/tab-level-funcs-actual.ts
@@ -0,0 +1,46 @@
+// holds actual implementations of tab level funcs
+
+/** if url contains this text, it is a exh image page (displays 1 image) */
+export const ExhImagePageUrl:string="exhentai.org/s";
+
+/** on all tabs, if it is an exh image tab, run exh open full size
+ *  page script */
+export async function exhOpenLargeImagesAll():Promise<void>
+{
+    const tabs:chrome.tabs.Tab[]=await chrome.tabs.query({
+        currentWindow:true,
+    });
+
+    for (var tab of tabs)
+    {
+        if (!tab.id)
+        {
+            console.error("tab had no id",tab);
+            continue;
+        }
+
+        if (!tab.url)
+        {
+            continue;
+        }
+
+        if (!tab.url.includes(ExhImagePageUrl))
+        {
+            continue;
+        }
+
+        await chrome.scripting.executeScript({
+            target:{tabId:tab.id},
+            func:()=>{
+                window.localStorage.setItem("args",JSON.stringify({
+                    runScript:"open-exh-full-size",
+                } satisfies CsArgs));
+            },
+        });
+
+        chrome.scripting.executeScript({
+            target:{tabId:tab.id},
+            files:["build-cs/schelper.iife.js"],
+        });
+    }
+}
--- web/lib/tab-level-funcs.ts
+++ web/lib/tab-level-funcs.ts
@@ -1,11 +1,11 @@
-// lib implementing tab level function collection
+// lib implementing tab level function collections and helpers for tab level funcs
 
 import _ from "lodash";
 import async from "async";
 
-/** if url contains this text, it is a exh image page (displays 1 image) */
-const ExhImagePageUrl:string="exhentai.org/s";
+import {ExhImagePageUrl,exhOpenLargeImagesAll} from "@/lib/tab-level-funcs-actual";
 
+/** collection of tab level funcs */
 const TabLevelFuncs:TabLevelFunc[]=[
     {
         name:"exh-open-all",
@@ -47,46 +47,4 @@ function oneTabWithUrl(url:string)
     }
 
     return inner;
-}
-
-/** on all tabs, if it is an exh image tab, run exh open full size
- *  page script */
-async function exhOpenLargeImagesAll():Promise<void>
-{
-    const tabs:chrome.tabs.Tab[]=await chrome.tabs.query({
-        currentWindow:true,
-    });
-
-    for (var tab of tabs)
-    {
-        if (!tab.id)
-        {
-            console.error("tab had no id",tab);
-            continue;
-        }
-
-        if (!tab.url)
-        {
-            continue;
-        }
-
-        if (!tab.url.includes(ExhImagePageUrl))
-        {
-            continue;
-        }
-
-        await chrome.scripting.executeScript({
-            target:{tabId:tab.id},
-            func:()=>{
-                window.localStorage.setItem("args",JSON.stringify({
-                    runScript:"open-exh-full-size",
-                } satisfies CsArgs));
-            },
-        });
-
-        chrome.scripting.executeScript({
-            target:{tabId:tab.id},
-            files:["build-cs/schelper.iife.js"],
-        });
-    }
 }
